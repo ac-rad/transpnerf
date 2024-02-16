d0fd59083944f856bbeba39c96bd93a7ab74294c
Nicole Streltsov <streltsovnicole@gmail.com>
2024-02-16 00:48:19 -0500

    try

--- transpnerf/transpnerf_datamanager.py
+++ transpnerf/transpnerf_datamanager.py
@@ -54,7 +54,9 @@ class TranspNerfDataManager(VanillaDataManager, Generic[TDataset]):
         super().__init__(
             config=config, device=device, test_mode=test_mode, world_size=world_size, local_rank=local_rank, **kwargs
         )
-    
+
+    # this causes CUDA out of memory
+        
     # def create_train_dataset(self) -> TDataset:
     #     """Sets up the data loaders for training"""
     #     # print("self.dataset_type -->", self.dataset_type)  # for now, hardcoding since the datset type is not changing
@@ -71,27 +73,27 @@ class TranspNerfDataManager(VanillaDataManager, Generic[TDataset]):
     #         scale_factor=self.config.camera_res_scale_factor,
     #     )
   
-    # def next_train(self, step: int) -> Tuple[RayBundle, Dict]:
-    #     """Returns the next batch of data from the train dataloader."""
-    #     self.train_count += 1
-    #     image_batch = next(self.iter_train_image_dataloader)
-    #     assert self.train_pixel_sampler is not None
-    #     assert isinstance(image_batch, dict)
-    #     batch = self.train_pixel_sampler.sample(image_batch)
-    #     ray_indices = batch["indices"]
-    #     ray_bundle = self.train_ray_generator(ray_indices)
+    def next_train(self, step: int) -> Tuple[RayBundle, Dict]:
+        """Returns the next batch of data from the train dataloader."""
+        self.train_count += 1
+        image_batch = next(self.iter_train_image_dataloader)
+        assert self.train_pixel_sampler is not None
+        assert isinstance(image_batch, dict)
+        batch = self.train_pixel_sampler.sample(image_batch)
+        ray_indices = batch["indices"]
+        ray_bundle = self.train_ray_generator(ray_indices)
 
-    #     # add on normal and depth metadata
-    #     #ray_bundle.metadata["depth"] = self._process_depth_normal_metadata(ray_indices, image_batch["depth_image"])
-    #     #ray_bundle.metadata["normal"] = self._process_depth_normal_metadata(ray_indices, image_batch["normal_image"])
+        # add on normal and depth metadata
+        #ray_bundle.metadata["depth"] = self._process_depth_normal_metadata(ray_indices, image_batch["depth_image"])
+        #ray_bundle.metadata["normal"] = self._process_depth_normal_metadata(ray_indices, image_batch["normal_image"])
         
-    #     return ray_bundle, batch
+        return ray_bundle, batch
 
-    # def _process_depth_normal_metadata(self, ray_indices: torch.Tensor, data: torch.Tensor) -> torch.tensor:
-    #     # this code is exactly what is happening in: 
-    #     # https://github.com/nerfstudio-project/nerfstudio/blob/45db2bcfabe6e0644a3a45a50ed80a9a685ddc34/nerfstudio/data/pixel_samplers.py#L239
+    def _process_depth_normal_metadata(self, ray_indices: torch.Tensor, data: torch.Tensor) -> torch.tensor:
+        # this code is exactly what is happening in: 
+        # https://github.com/nerfstudio-project/nerfstudio/blob/45db2bcfabe6e0644a3a45a50ed80a9a685ddc34/nerfstudio/data/pixel_samplers.py#L239
 
-    #     c, y, x = (i.flatten() for i in torch.split(ray_indices, 1, dim=-1))
-    #     c, y, x = c.cpu(), y.cpu(), x.cpu()
-    #     return data[c, x, y]
+        c, y, x = (i.flatten() for i in torch.split(ray_indices, 1, dim=-1))
+        c, y, x = c.cpu(), y.cpu(), x.cpu()
+        return data[c, x, y]
 
